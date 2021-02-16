982f656f3b10fcfa870e1309252b1d43884d026a
DESKTOP-EF8F0M9\yevhe <yevheniia.vyshnevska@jmind.systems>
2021-02-16 18:38:28 +0200

    first

---
+++ Lerning.py
@@ -0,0 +1,18 @@
+number_list = [1, 0, -52, 3, 5, -8, 13, 21, 34, -55, 89]
+for number in number_list:
+
+    if number < 5:
+        print(number)
+
+def print_hi():
+    # Use a breakpoint in the code line below to debug your script.
+
+    number_list = [1, 0, -52, 3, 5, -8, 13, 21, 34, -55, 89]
+    for number in number_list:
+        if number < 5:
+            print(number)
+
+
+list_words = []
+for a in my_text.split():
+    for
